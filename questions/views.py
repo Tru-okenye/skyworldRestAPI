8bd6be6895b249bcc3b95c78affd362e397f3a8e
Truphy <truphenaokenye@gmail.com>
2023-11-10 20:31:05 +0300

    added pagination

--- questions/views.py
+++ questions/views.py
@@ -6,19 +6,27 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from .serializers import QuestionSerializer, ResponseSerializer
 from .models import Question, Response
+from rest_framework.pagination import PageNumberPagination
 
 class QuestionView(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 10  # You can adjust the page size as needed
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
 class ResponseView(viewsets.GenericViewSet, mixins.UpdateModelMixin, mixins.ListModelMixin):
     queryset = Response.objects.all()
     serializer_class = ResponseSerializer
+    pagination_class = CustomPageNumberPagination
 
     def perform_update(self, serializer):
         question_id = self.request.data.get('question')
         question = Question.objects.get(pk=question_id)
         serializer.save(question=question)
+        
 
     @action(detail=False, methods=['get'])
     def filter_by_email(self, request):
